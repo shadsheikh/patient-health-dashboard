120bd5fc7c61f3026d0633b266bccbaf5eee48d7
Shad Sheikh <shadsheikh3107@gmail.com>
2024-10-24 14:06:25 +0530

    config

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors());
+const corsOption = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+app.use(cors(corsOption));
+
 const PORT = process.env.PORT || 3000
 
 // Get all patients
